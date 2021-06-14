1b6c228e0ca4b5fbfb7b480d43c79ebc962299b2
thushartom <thushartom@gmail.com>
2021-06-14 12:19:34 +0530

    Title Extraction complete

--- titleExtraction.py
+++ titleExtraction.py
@@ -1,7 +1,7 @@
 import requests
-import bs4 
+import bs4
 
-req = requests.get("https://advisor.foundingminds.in/")
+req = requests.get("https://dev.otw.redtailtechnology.com/dashboard/overview")
 
 # print(req.text)
 soup = bs4.BeautifulSoup(req.text, 'html.parser')
